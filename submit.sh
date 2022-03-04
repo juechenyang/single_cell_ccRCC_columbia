426e7be179ff65b5d9920e195841c0f8d38835d2
Juechen <juechen.yang@gmail.com>
2022-03-04 15:17:50 -0500

    update submit.sh

--- submit.sh
+++ submit.sh
@@ -1,15 +1,15 @@
-while getopts c:n:m: flag
+while getopts f:n:m: flag
 do
     case "${flag}" in
-        c) command=${OPTARG};;
+        f) rfile=${OPTARG};;
         n) nThread=${OPTARG};;
         m) memory=${OPTARG};;
     esac
 done
 echo "number of thread: $nThread";
 echo "memory: $memory"
-echo "command is: $command"
+echo "command is: $rfile"
 
 bsub -M $memory -n $nThread -W 120:00 -R "span[hosts=1]" \
 -J jason -o /users/yanv5j/logs/%J.out -e /users/yanv5j/logs/%J.err \
-"$command"
+"Rscript $rfile"
