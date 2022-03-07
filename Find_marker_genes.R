1f86b480577d845d5a8582c6bc0c72b86a4d59d9
Juechen <juechen.yang@gmail.com>
2022-03-07 13:22:21 -0500

    update to top50 genes

--- Find_marker_genes.R
+++ Find_marker_genes.R
@@ -2,9 +2,10 @@ source("call_libraries.R")
 source("Initialization.R")
 
 integrated_cancer_cells = readRDS("integrated_cancer_cells.rds")
+Idents(integrated_cancer_cells) = as.character(integrated_cancer_cells$integrated_snn_res.0.2)
 all_markers = FindAllMarkers(integrated_cancer_cells, only.pos = TRUE, 
                              min.pct = 0.25, logfc.threshold = 0.25)
-top_n = 10
+top_n = 50
 top_n_markers = all_markers %>% group_by(cluster) %>% top_n(n = top_n, wt = avg_log2FC)
 
 # output top n markers
