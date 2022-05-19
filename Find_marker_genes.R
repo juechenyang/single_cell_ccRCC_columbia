f18f178a9b27a0fedbf7e945f26f969060f7ac0f
Juechen <juechen.yang@gmail.com>
2022-05-19 16:47:27 -0400

    update gsea for same cluster diff stage

--- Find_marker_genes.R
+++ Find_marker_genes.R
@@ -5,6 +5,8 @@ source("gsea_tools.R")
 integrated_cancer_cells = readRDS("integrated_cancer_cells.rds")
 DefaultAssay(integrated_cancer_cells) = "RNA"
 integrated_cancer_cells = NormalizeData(integrated_cancer_cells)
+integrated_cancer_cells$stage = 
+  ifelse(integrated_cancer_cells$patient=="Patient5", "Metastatic", integrated_cancer_cells$stage)
 #Idents(integrated_cancer_cells) = as.character(integrated_cancer_cells$integrated_snn_res.0.2)
 plot_list = list()
 for(x in 0:9){
@@ -34,7 +36,30 @@ ggarrange(
 dev.off()
 
 
+####################################################################################
+###Compare cluster 4 between stage 1 and stage 3
+####################################################################################
+for(x in c("pT1b","pT3a","Metastatic")){
+  integrated_cancer_cell_stage = subset(integrated_cancer_cell, subset = stage == x)
+  
+}
 
+cluster_number = 2
+stage1_cluster <- (integrated_cancer_cells$integrated_snn_res.0.2==cluster_number & integrated_cancer_cells$stage == "pT1b")
+stage3_cluster <- (integrated_cancer_cells$integrated_snn_res.0.2==cluster_number & integrated_cancer_cells$stage == "pT3a")
+mat = data.frame(integrated_cancer_cells[["RNA"]]@data, check.names = F)
+t_value = apply(mat, MARGIN = 1, FUN = function(x){
+  stage1 = x[stage1_cluster]
+  stage3 = x[stage3_cluster]
+  t_result = t.test(stage3, stage1)
+  return(t_result$statistic)
+})
+gsea_obj = run_gsea_kegg(t_value)
+png("gsea_cluster2_stage3_vs_stage1.png", width = 10, height = 10, units = "in", res = 300)
+dotplot(gsea_obj, showCategory = 10, split=".sign")+ 
+        facet_grid(.~.sign)+
+        ggtitle("cluster2 stage3 vs stage1")
+dev.off()
 
 
 
